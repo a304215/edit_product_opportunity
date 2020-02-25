6cfe92c1ef137bd2fb6e84e2944a1e169eae02b0
a304215 <andy940047@gmail.com>
2020-02-25 23:25:26 +0800

    new code

--- force-app/main/default/lwc/edit_product/edit_product.js
+++ force-app/main/default/lwc/edit_product/edit_product.js
@@ -3,13 +3,23 @@ import get_all_product from '@salesforce/apex/EditProduct.get_all_product';
 export default class Edit_product extends LightningElement {
     @api recordId;
     @track edit_product = false;
+    @track edit_product_display_list = [];
     handleButtonSelect(){
-        this.edit_product = true;
         get_all_product({opp_id:this.recordId})
         .then(result => {
-            console.log(result);
-            this.product = result;
-            do_for();
+            for(let i = 0 ; i < result.length;i++){
+                this.edit_product_display_list[i] = {
+                    name:result[i].Name,
+                    quantity:result[i].Quantity,
+                    unitprice:result[i].UnitPrice.toFixed(2).toString(),
+                    discount:result[i].Discount.toString(),
+                    nameUrl:'/lightning/r/PricebookEntry/'+result[i].Id,
+                    sale_price:result[i].UnitPrice.toFixed(2).toString(),
+                    total:result[i].UnitPrice.toFixed(2).toString()
+                }
+            }
+            console.log(this.edit_product_display_list);
+            this.edit_product = true
         })
         .catch(error => {
             this.error = error;
